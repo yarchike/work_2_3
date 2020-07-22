4670f724e69e608af07492177f1d9dd47f68cca7
Yarchik <yarchike@gmail.com>
2020-07-22 20:30:21 +0300

    Добавил загрузку из файла

--- app/src/main/java/com/yarchike/work_2_1/PostData.kt
+++ app/src/main/java/com/yarchike/work_2_1/PostData.kt
@@ -1,5 +1,6 @@
 package com.yarchike.work_2_1
 
+import android.util.Log
 import io.ktor.client.HttpClient
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
@@ -7,43 +8,50 @@ import io.ktor.client.request.get
 import io.ktor.http.ContentType
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
 
 class PostData {
 
     companion object {
 
 
-
         fun getDataBase(): ArrayList<Post> {
+
             val list = getUrl()
-            return list
+            return  list
         }
 
 
+        fun getUrl(): ArrayList<Post> {
+            var result = ArrayList<Post>()
+            val client = HttpClient {
+                install(JsonFeature) {
+                    acceptContentTypes = listOf(
+                        ContentType.Text.Plain,
+                        ContentType.Application.Json
+                    )
+                    serializer = GsonSerializer()
+                }
 
-     fun getUrl():ArrayList<Post> {
-         var result = ArrayList<Post>()
-        val client = HttpClient{
-            install(JsonFeature){
-                acceptContentTypes = listOf(
-                    ContentType.Text.Plain,
-                    ContentType.Application.Json
-                )
-                serializer = GsonSerializer()
             }
+            with(CoroutineScope(IO)) {
+                launch {
+                    val response: ArrayList<Post> =
+                        client.get<ArrayList<Post>>(urlString = "https://raw.githubusercontent.com/yarchike/work_2_2_toJSON/master/posts.json")
+                    Log.d("Myv", response.toString())
+                    result = response
+                    Log.d("Myv", result.toString())
+                }
+
+
+            }
+            return result
 
         }
-         with(CoroutineScope(IO)){
-             launch {
-                 val response: ArrayList<Post> = client.get<ArrayList<Post>>(urlString = "https://raw.githubusercontent.com/yarchike/work_2_2_toJSON/master/posts.json")
-                 result = response
-             }
 
-         }
-         return  result
+
     }
-    }
-
-
 }
